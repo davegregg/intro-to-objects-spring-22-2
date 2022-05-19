fe953cb7f24e1a88e7aafae36ee7fb02bfae20b8
davegregg@gmail.com <davegregg@gmail.com>
2022-05-19 12:01:14 -0400

    Topic session demo code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# intro-to-objects-spring-22-2
+# Intro to Objects
+
+## What sorts of information can we represent using *Booleans*?
+
+- true/false: affirming or rejecting something, checking the correctness of something
+- on/off
+- proceed/end
+- 0 and 1
+- Numbers (01001101: 77)
+- Strings
+- Everything else.
+
+## What sorts of information can we represent using *Numbers*?
+
+- Strings, etc.
+
+## What sorts of information can we represent using *Strings*?
+
+- Text
+- Human emotion
+- Code
+
+## What sorts of information can we represent using *Arrays*?
+
+- Numbers
+- Strings
+- Any list of information
+
+## What sorts of information can we represent using *Objects*?
+
+- ~~Infinity~~ Anything describable.
+- Anything by its qualities/properties.
+- Any list of information.
+- Collections of tools.
+- Physical objects.
+- Form data. (User: username, email, phone, etc.)
+- SEE EXAMPLES in `main.js`.
---
+++ main.js
@@ -0,0 +1,54 @@
+const userFormData = {
+    username: "kenyatta_the_barbarian",
+    email: "kenyatta97@hotmail.com",
+    age: 68,
+    favoriteBooks: ["Harry Potter", "Goodnight, Moon", "The Way of Suffering: A Geography of Crisis"],
+}
+
+let mrBubz = {
+    species: "dog",
+    eyeDirections: 2,
+    anger: Infinity,
+}
+
+const console = {
+    log: function log (...data) { 
+        document.body.append(...data)
+    },
+    assert: function (assertion, msg, ...substrings) { /* ... */ },
+    dir: function (obj) { /* ... */ },
+}
+
+console.log("hahaha", "I GOT YOU", "Mr. Bubz")
+
+let pets = {
+    0: "Mr. Bubz",
+    1: "Garfield",
+    2: "Grumpy Cat",
+    3: "Wilfred",
+    4: "Gabe the Dog",
+    5: "Lil Bub",
+    length: 6,
+}
+
+for (let index = 0; index < pets.length; index += 1) {
+   const currentPet = pets[index] 
+
+   console.log(currentPet)
+}
+
+// pets.forEach(pet => console.log(pet))
+
+// All Arrays are Objects. But not all Objects are Arrays.
+// Arrays are Objects with extra added tools and behavior.
+// Plain Objects ("Plain Old JavaScript Objects," a.k.a "POJOs" 
+// are just generic objects).
+
+
+
+// ACCESSING PROPERTIES ON AN EXISTING OBJECT:
+console.log(user.username);  // Dot notation
+console.log(user["username"]);  // Bracket notation
+
+let requestedData = "username";
+console.log(user[requestedData]);  // Bracket notation again
